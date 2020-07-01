6d599b0e420c022140dd6339d66eee32fd929ed4
didrikseni <iandidri@gmail.com>
2020-07-01 14:10:29 -0300

    Changes in api routes

--- app/Http/Controllers/API/APIHelperController.php
+++ app/Http/Controllers/API/APIHelperController.php
@@ -10,13 +10,4 @@ class APIHelperController extends Controller
     public function show() {
         return request()->user();
     }
-
-    public function getRoutes() {
-        Route::post('login', 'API\AuthController@login')->name('login');
-        Route::post('register', 'API\AuthController@register');
-        Route::group(['middleware' => 'auth:api'], function () {
-            Route::get('logout', 'API\AuthController@logout');
-            Route::get('user', 'API\AuthController@user');
-        });
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', 'API\APIHelperController@show');
 
-Route::post('/register', 'Api\AuthController@register');
-Route::post('/login', 'Api\AuthController@login');
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
 
 Route::resource('/api_articles', 'API\ApiArticleController')->middleware('auth:api');
 Route::resource('/api_users', 'API\ApiUserController')->middleware('auth:api');
