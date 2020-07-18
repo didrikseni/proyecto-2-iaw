02547c760270b745a4f59dc57941fd5be36c688f
didrikseni <iandidri@gmail.com>
2020-07-17 23:13:31 -0300

    Changes in api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,11 +16,8 @@ class AuthController extends Controller
             'password' => 'required|confirmed'
         ]);
         $validatedData['password'] = bcrypt($request->password);
-
         $user = User::create($validatedData);
-
         $accessToken = $user->createToken('authToken')->accessToken;
-
         return response(json_encode(['userData' =>[ 'user' => $user, 'access_token' => $accessToken]]), 200);
     }
 
@@ -33,21 +30,19 @@ class AuthController extends Controller
             return response(['message' => 'Invalid Credentials'], 401);
         }
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
-
         return response(json_encode(['userData' => ['user' => auth()->user(), 'access_token' => $accessToken]]), 200);
     }
 
     public function logged_in(Request $request) {
-        if (DB::table('oauth_access_tokens')->where('user_id', '=', $request->user_id)->exists()){
-            return response(json_encode(['userData' => auth()->user()]), 200);
+        if (Auth::guard('api')->check()) {
+            return response()->json(['logged_in' => true], 200);
         } else {
-            return response(['message' => 'Not logged in'], 418);
+            return response()->json(['logged_in' => false], 418);
         }
     }
 
     public function logout(Request $request) {
         $request->user()->token()->revoke();
         return response()->json(['message' => 'Successfully logged out']);
-}
-
+    }
 }
